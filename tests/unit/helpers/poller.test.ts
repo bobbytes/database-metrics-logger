e66e1fafaafe260f0f4ea1d8ae1dc92582213e57
Robert Kleger <robert.kleger@dormakaba.com>
2019-01-28 10:10:37 +0100

    ✅  set poller coverage 100%

--- tests/unit/helpers/poller.test.ts
+++ tests/unit/helpers/poller.test.ts
@@ -3,13 +3,23 @@ import { Poller } from '../../../src/helpers/poller';
 describe('Poller', () => {
   let poller: Poller;
 
-  beforeEach(() => {
-    poller = new Poller({ interval: 2000 });
-  });
-
   jest.useFakeTimers();
 
-  test('callback must be called', () => {
+  test('callback must be called with interval config', () => {
+    poller = new Poller({ interval: 2000 });
+    expect(poller).toBeInstanceOf(Poller);
+
+    const pollerCallback = jest.fn();
+    poller.onPoll(pollerCallback.bind(this));
+    poller.poll();
+
+    expect(pollerCallback).not.toBeCalled();
+    jest.runAllTimers();
+    expect(pollerCallback).toBeCalled();
+  });
+
+  test('callback must be called without interval config', () => {
+    poller = new Poller();
     expect(poller).toBeInstanceOf(Poller);
 
     const pollerCallback = jest.fn();
