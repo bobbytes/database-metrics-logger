2f8516f793392a8588c915cd6a7f1bcdddb5310f
Robert Kleger <robert.kleger@dormakaba.com>
2019-06-28 13:10:37 +0200

    🚨 remove tslint errors

--- src/modules/database-metrics/mongodb/helpers/get-replication-set-members-count.ts
+++ src/modules/database-metrics/mongodb/helpers/get-replication-set-members-count.ts
@@ -1,4 +1,2 @@
-import { ReplicationSetMemberState } from '../enums/replication-set-member-state.enum';
-
 export const getReplicationSetMembersCount = replicationSetStatus =>
   replicationSetStatus && replicationSetStatus.members && replicationSetStatus.members.length || 0;
